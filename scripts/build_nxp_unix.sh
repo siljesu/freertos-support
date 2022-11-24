4bbdec8174a4ed1ff7f897693da4bfb11f5c9e93
Silje Susort <silje.susort@gmail.com>
2022-11-24 11:36:19 +0100

    Add two-step build process for experimentation

--- scripts/build_nxp_unix.sh
+++ scripts/build_nxp_unix.sh
@@ -22,13 +22,3 @@ cp $PROJECT_ROOT/platform/platform.h $LF_SOURCE_GEN_DIRECTORY/include/core/
 cp $PROJECT_ROOT/platform/reactor.c $LF_SOURCE_GEN_DIRECTORY/include/core/
 cp $PROJECT_ROOT/platform/reactor_common.c $LF_SOURCE_GEN_DIRECTORY/include/core/
 
-# Build project
-cd $PROJECT_ROOT
-
-if [ -d "CMakeFiles" ];then rm -rf CMakeFiles; fi
-if [ -f "Makefile" ];then rm -f Makefile; fi
-if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
-if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
-cmake -DCMAKE_TOOLCHAIN_FILE="armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
-make -j 2>&1 | tee build_log.txt
-
